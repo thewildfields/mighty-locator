f86d7fc3ef466a589604109716879ccc1cec122b
oleksii-twf <146258454+oleksii-twf@users.noreply.github.com>
2024-05-21 11:40:23 -0700

    Adding Listings form

--- wp-content/themes/mighty-locator/archive-listing.php
+++ wp-content/themes/mighty-locator/archive-listing.php
@@ -1,4 +1,32 @@
-<?php get_header(); ?>
+<?php
+
+
+$listingByAuthor = get_posts('post_type=listing&author='.get_current_user_id() );
+
+$canCreateListing = sizeof( $listingByAuthor ) === 0 ? true : false;
+
+if( isset( $_POST ) && !empty( $_POST ) ){
+								
+	wp_insert_post(
+		wp_slash(
+			[
+				'post_type' => 'listing',
+				'author' => get_current_user_id(),
+				'post_title' => $_POST['post-name'],
+				'post_content' => $_POST['post-content'],
+				'post_status' => 'publish',
+				'meta_input' => [
+					'price_range' => $_POST['listing-price']
+				]
+			]
+		)
+	);
+
+}
+
+get_header();
+
+?>
 
 <div class="app-wrapper">
     <div class="app-content pt-3 p-md-3 p-lg-4">
@@ -11,7 +39,37 @@
 				<div class="container" style="padding-left: 0; padding-right: 0">
 					<div class="row justify-content-center d-flex">
 						<div class="col-lg-8 post-list">
-                            <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+							<?php if( $canCreateListing ){ ?>
+
+								<h4 style="margin: 30px 0;">Create your own listing!</h4>
+
+								<form class="single-post d-flex flex-row form-area" method="POST" action="#">
+									<div class="thumb">
+											<img src="<?php echo get_avatar_url( get_current_user_id() ); ?>" alt="">
+									</div>
+									<div class="details">
+										<div class="title d-flex flex-row justify-content-between">
+											<div class="titles">
+												<input class="common-input form-control" type="text" name="post-name" placeholder="Listing name" id="">
+												<h6><?php?></h6>					
+											</div>
+										</div>
+										<div>
+											<textarea class="common-input form-control" name="post-content" name="listing-content"></textarea>
+										</div>
+										<p class="address">
+											<input class="common-input form-control" type="text" name="listing-price" placeholder="Price or Price range" required>
+										</p>
+										<input type="submit" value="Submit">
+									</div>
+								</form>	
+
+                            <?php
+
+								}; 
+							
+							if( have_posts() ) : while( have_posts() ) : the_post(); ?>
                                 <div class="single-post d-flex flex-row">
                                     <div class="thumb">
 										<?php if( get_the_post_thumbnail() ) { ?>
@@ -19,23 +77,14 @@
 										<?php } else { ?>
                                         	<img src="<?php echo get_avatar_url( get_the_author_meta('ID') ); ?>" alt="">
 										<?php } ?>
-                                        <ul class="tags">
-											<?php
-												$tags = get_the_terms( get_the_id(), 'post_tag' );
-												foreach ( $tags as $tag ) {
-													echo '<li><a>' . $tag->name . '</a></li>';
-												}
-											?>
-                                        </ul>
                                     </div>
                                     <div class="details">
                                         <div class="title d-flex flex-row justify-content-between">
                                             <div class="titles">
-                                                <a href="single.html"><h4><?php the_title(); ?></h4></a>
+                                                <h4><?php the_title(); ?></h4>
                                                 <h6><?php the_author(); ?></h6>					
                                             </div>
                                             <ul class="btns">
-                                                <li><a><span class="lnr lnr-heart"></span></a></li>
                                                 <li><a>Contact</a></li>
                                             </ul>
                                         </div>
@@ -45,21 +94,7 @@
                                 </div>
                             <?php endwhile; endif; ?>
 						</div>
-						<div class="col-lg-4 sidebar">
-							<div class="single-slidebar">
-								<h4>Listings by Location</h4>
-								<ul class="cat-list">
-									<li><a class="justify-content-between d-flex" href="category.html"><p>New York</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Park Montana</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Atlanta</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Arizona</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Florida</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Rocky Beach</p></a></li>
-									<li><a class="justify-content-between d-flex" href="category.html"><p>Chicago</p></a></li>
-								</ul>
-							</div>		
-
-						</div>
+						<div class="col-lg-4 sidebar"></div>
 					</div>
 				</div>	
 			</section>
